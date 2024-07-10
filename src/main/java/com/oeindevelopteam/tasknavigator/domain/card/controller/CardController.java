4be71d335788362007f3eccee15ccce39c84bc21
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-10 21:10:24 +0900

    ✨ update - #10 카드 생성 api 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/card/controller/CardController.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/card/controller/CardController.java
@@ -1,5 +1,27 @@
 package com.oeindevelopteam.tasknavigator.domain.card.controller;
 
+import com.oeindevelopteam.tasknavigator.domain.card.dto.CardRequestDto;
+import com.oeindevelopteam.tasknavigator.domain.card.dto.CardResponseDto;
+import com.oeindevelopteam.tasknavigator.domain.card.service.CardService;
+import com.oeindevelopteam.tasknavigator.global.dto.CommonResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CardController {
 
+  private final CardService cardService;
+
+  @PostMapping("/boards/{boardId}/columns/{columnsId}/cards")
+  public ResponseEntity<CommonResponseDto> createCard(@RequestBody CardRequestDto cardRequestDto) {
+    CardResponseDto responseDto = cardService.createdCard(cardRequestDto);
+
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(new CommonResponseDto(200, "카드 생성에 성공하였습니다.", responseDto));
+  }
 }
