e6b31477d1c711860314b628eaa5a8ed196593ae
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-10 20:29:50 +0900

    ✨ update - #10 CardResponseDto 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/card/dto/CardResponseDto.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/card/dto/CardResponseDto.java
@@ -1,5 +1,26 @@
 package com.oeindevelopteam.tasknavigator.domain.card.dto;
 
+import com.oeindevelopteam.tasknavigator.domain.card.entity.Card;
+import lombok.Getter;
+
+@Getter
 public class CardResponseDto {
 
+  private Long cardId;
+  private Long userId;
+  private Long columnId;
+  private String title;
+  private String content;
+  private String dueDate;
+  private String manager;
+
+  public CardResponseDto(Card card) {
+    this.cardId = card.getId();
+    this.userId = card.getUserId();
+    this.columnId = card.getColumnId();
+    this.title = card.getTitle();
+    this.content = card.getContent();
+    this.dueDate = card.getDueDate();
+    this.manager = card.getManager();
+  }
 }
