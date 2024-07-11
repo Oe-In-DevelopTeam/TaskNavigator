93ba7cff276c88ccb548e549e9b9ecf3b5fc1007
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-11 21:54:08 +0900

    ♻️ refactor - #36 Card와 CardTag를 CardTagMatches테이블에 OneToMany로 연관, 복합키 설정

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/card/entity/Card.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/card/entity/Card.java
@@ -1,12 +1,16 @@
 package com.oeindevelopteam.tasknavigator.domain.card.entity;
 
 import com.oeindevelopteam.tasknavigator.domain.card.dto.CardRequestDto;
+import com.oeindevelopteam.tasknavigator.global.entity.Timestamped;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.Set;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "cards")
 @NoArgsConstructor
-public class Card {
+public class Card extends Timestamped {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +42,9 @@ public class Card {
   @Column
   private String manager;
 
+  @OneToMany(mappedBy = "card", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<CardTagMatches> tagMatches;
+
   public Card(CardRequestDto cardRequestDto, Long columnId, Long userId) {
     this.userId = userId;
     this.columnId = columnId;
@@ -60,5 +67,9 @@ public class Card {
     if (cardRequestDto.getManager() != null) {
       this.manager = cardRequestDto.getManager();
     }
+  }
+
+  public void setTagMatches(Set<CardTagMatches> tagMatches) {
+    this.tagMatches = tagMatches;
   }
 }
