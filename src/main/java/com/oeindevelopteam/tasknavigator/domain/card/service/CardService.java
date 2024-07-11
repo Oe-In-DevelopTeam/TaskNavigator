df1c3dea9e01cabb01d3a09348776d68837ca149
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-11 10:38:55 +0900

    ✨ update - #10 카드 저장 로직 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/card/service/CardService.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/card/service/CardService.java
@@ -1,5 +1,27 @@
 package com.oeindevelopteam.tasknavigator.domain.card.service;
 
+import com.oeindevelopteam.tasknavigator.domain.card.dto.CardRequestDto;
+import com.oeindevelopteam.tasknavigator.domain.card.dto.CardResponseDto;
+import com.oeindevelopteam.tasknavigator.domain.card.entity.Card;
+import com.oeindevelopteam.tasknavigator.domain.card.repository.CardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CardService {
 
+  private final CardRepository cardRepository;
+
+  public CardResponseDto createdCard(CardRequestDto cardRequestDto, Long columnId) {
+
+    // TODO: 유저아이디 넣어주는거 필요
+    Long userId = 1L;
+
+    Card card = new Card(cardRequestDto, columnId, userId);
+
+    cardRepository.save(card);
+
+    return new CardResponseDto(card);
+  }
 }
