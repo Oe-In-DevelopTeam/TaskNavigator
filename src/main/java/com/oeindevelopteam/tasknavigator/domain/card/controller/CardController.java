d18b9c9c123f955556691dfe48f9e6bdf5a372c1
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-11 10:39:22 +0900

    ✨ update - #10 카드 저장 api 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/card/controller/CardController.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/card/controller/CardController.java
@@ -7,6 +7,7 @@ import com.oeindevelopteam.tasknavigator.global.dto.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,11 @@ public class CardController {
 
   private final CardService cardService;
 
-  @PostMapping("/boards/{boardId}/columns/{columnsId}/cards")
-  public ResponseEntity<CommonResponseDto> createCard(@RequestBody CardRequestDto cardRequestDto) {
-    CardResponseDto responseDto = cardService.createdCard(cardRequestDto);
-
+  @PostMapping("/boards/{boardId}/columns/{columnId}/cards")
+  public ResponseEntity<CommonResponseDto> createCard(@PathVariable Long boardId,
+      @PathVariable Long columnId, @RequestBody CardRequestDto cardRequestDto) {
+    CardResponseDto responseDto = cardService.createdCard(cardRequestDto, columnId);
+    // TODO: AOP 사용해서 boardId, columnId 확인
     return ResponseEntity.status(HttpStatus.OK)
         .body(new CommonResponseDto(200, "카드 생성에 성공하였습니다.", responseDto));
   }
