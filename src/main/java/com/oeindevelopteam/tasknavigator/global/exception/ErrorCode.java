f585937c6f58a06bccc778182d592567740814b4
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-12 14:38:39 +0900

    ✨ update - #12 카드 태그를 찾을 수 없는 커스텀 에러 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/global/exception/ErrorCode.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/global/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
   FAIL(500, "실패했습니다."),
-  CARD_NOT_FOUND(404, "해당 카드를 찾을 수 없습니다.");
+  CARD_NOT_FOUND(404, "해당 카드를 찾을 수 없습니다."),
+  CARD_TAG_NOT_FOUND(404, "해당 카드 태그를 찾을 수 없습니다.");
 
   private int status;
   private String message;
