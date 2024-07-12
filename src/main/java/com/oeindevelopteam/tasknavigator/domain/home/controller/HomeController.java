67f0ba04ee0bada90cb9dc18060254f8ee536fdd
chamcham0707 <cheamin6306@naver.com>
2024-07-12 22:39:47 +0900

    ♻️ refactor - #56 create-board.html 페이지 연동

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/home/controller/HomeController.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/home/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
   public String homePage() {
     return "index";
   }
+
+  @GetMapping("/home/newBoard")
+  public String createBoardPage() {
+    return "create-board";
+  }
 }
