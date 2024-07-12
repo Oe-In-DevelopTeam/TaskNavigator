2900446366501b34b98a30fe6381e18e9f8bd241
nenney <a01056499749@gmail.com>
2024-07-13 04:10:39 +0900

    ✨ update - #61 Refresh Token 재발급 기능 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/user/service/UserService.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/user/service/UserService.java
@@ -9,11 +9,20 @@ import com.oeindevelopteam.tasknavigator.domain.user.repository.UserRoleReposito
 import com.oeindevelopteam.tasknavigator.domain.user.security.UserDetailsImpl;
 import com.oeindevelopteam.tasknavigator.global.exception.CustomException;
 import com.oeindevelopteam.tasknavigator.global.exception.ErrorCode;
+import com.oeindevelopteam.tasknavigator.global.jwt.JwtProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserService {
@@ -22,16 +31,19 @@ public class UserService {
   private final UserRoleRepository userRoleRepository;
   private final PasswordEncoder passwordEncoder;
   private final UserRoleMatchesRepository userRoleMatchesRepository;
+  private final JwtProvider jwtProvider;
 
   @Value("${admin.token}")
   private String adminToken;
 
   public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository,
-      PasswordEncoder passwordEncoder, UserRoleMatchesRepository userRoleMatchesRepository) {
+      PasswordEncoder passwordEncoder, UserRoleMatchesRepository userRoleMatchesRepository,
+      JwtProvider jwtProvider) {
     this.userRepository = userRepository;
     this.userRoleRepository = userRoleRepository;
     this.passwordEncoder = passwordEncoder;
     this.userRoleMatchesRepository = userRoleMatchesRepository;
+    this.jwtProvider = jwtProvider;
   }
 
   @Transactional
@@ -71,6 +83,74 @@ public class UserService {
 
     user.updateRefreshToken(null);
     userRepository.save(user);
+  }
+
+  @Transactional
+  public HttpHeaders refreshToken(HttpServletRequest request) {
+    String token = getRefreshToken(request);
+
+    if (!StringUtils.hasText(token)) {
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+
+    return validateToken(token);
+  }
+
+  private String getRefreshToken(HttpServletRequest request) {
+
+    Cookie[] cookies = request.getCookies();
+
+    if (cookies == null) {
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+
+    for (Cookie el : cookies) {
+      if ("refreshToken".equals(el.getName())) {
+        return el.getValue();
+      }
+    }
+
+    return null;
+  }
+
+  private HttpHeaders validateToken(String token) {
+
+    try {
+
+      Claims info = jwtProvider.getClaimsFromToken(token);
+      User user = userRepository.findByUserId(info.getSubject())
+          .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+      if (!user.getRefreshToken().equals(token)) {
+        throw new CustomException(ErrorCode.INVALID_TOKEN);
+      }
+
+      return setHeaders(info, user);
+
+    } catch (ExpiredJwtException e) {
+      throw new CustomException(ErrorCode.EXPIRED_TOKEN);
+    } catch (JwtException e) {
+      throw new CustomException(ErrorCode.INVALID_TOKEN);
+    }
+
+  }
+
+  private HttpHeaders setHeaders(Claims info, User user) {
+
+    String accessToken = jwtProvider.createAccessToken(user.getUserId(), user.getRole());
+    String refreshToken = jwtProvider.generateToken(user.getUserId(), user.getRole(),
+        info.getExpiration());
+    ResponseCookie responseCookie = jwtProvider.createCookieRefreshToken(refreshToken,
+        info.getExpiration());
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", "Bearer " + accessToken);
+    headers.add(HttpHeaders.SET_COOKIE, responseCookie.toString());
+
+    user.updateRefreshToken(refreshToken);
+
+    return headers;
+
   }
 
 }
