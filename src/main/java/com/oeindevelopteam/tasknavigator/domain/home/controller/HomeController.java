bd1a553e95e57eb8edc28f0ef2070de4b3623e6f
chamcham0707 <cheamin6306@naver.com>
2024-07-12 22:52:09 +0900

    ♻️ refactor - #56 edit-card.html & card-details.html 페이지 연동

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/home/controller/HomeController.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/home/controller/HomeController.java
@@ -2,17 +2,33 @@ package com.oeindevelopteam.tasknavigator.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-  @GetMapping("/home")
+  @GetMapping()
   public String homePage() {
     return "index";
   }
 
-  @GetMapping("/home/newBoard")
+  @GetMapping("/newBoard")
   public String createBoardPage() {
     return "create-board";
+  }
+
+  // TODO: 카드 번호에 따라 다른 상세 페이지를 보여주는 것이 필요
+//  @GetMapping("/cards/{cardId}")
+  @GetMapping("/cards")
+  public String cardDetailsPage() {
+    return "card-details";
+  }
+
+  // TODO: 카드 번호에 따라 다른 수정 페이지를 보여주는 것이 필요
+//  @GetMapping("/cards/edit/{cardId}")
+  @GetMapping("/cards/edit")
+  public String cardEditPage() {
+    return "edit-card";
   }
 }
