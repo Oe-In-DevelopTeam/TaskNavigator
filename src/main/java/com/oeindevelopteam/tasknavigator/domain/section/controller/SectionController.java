9ea626978b3475476e7e6367b2fce6ae458b1655
chamcham0707 <cheamin6306@naver.com>
2024-07-12 18:48:20 +0900

    ✨ update - #39 컬럼 삭제 기능 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/section/controller/SectionController.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/section/controller/SectionController.java
@@ -7,6 +7,7 @@ import com.oeindevelopteam.tasknavigator.global.dto.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,4 +29,10 @@ public class SectionController {
         .body(new CommonResponseDto(HttpStatus.OK.value(), "컬럼이 생성되었습니다.", responseDto));
   }
 
+  @DeleteMapping("/{columnId}")
+  ResponseEntity<CommonResponseDto> deleteSection(@PathVariable Long boardId, @PathVariable Long columnId) {
+    sectionService.deleteSection(boardId, columnId);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(new CommonResponseDto(HttpStatus.OK.value(), "컬럼이 삭제되었습니다.", null));
+  }
 }
--- src/main/java/com/oeindevelopteam/tasknavigator/domain/section/entity/SectionStatus.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/section/entity/SectionStatus.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -16,6 +17,7 @@ public class SectionStatus {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @NotNull(message = "sectionStatus에 상태값은 필수입니다.")
   private String status;
 
   @ManyToOne
