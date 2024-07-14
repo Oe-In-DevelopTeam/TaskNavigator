9851451134ae773450894f2ded86c2bd30a59149
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-14 21:01:28 +0900

    ✨ update - #16 UserBoardMatchesRepository 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/board/repository/UserBoardMatchesRepository.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/board/repository/UserBoardMatchesRepository.java
@@ -1,12 +1,16 @@
 package com.oeindevelopteam.tasknavigator.domain.board.repository;
 
 import com.oeindevelopteam.tasknavigator.domain.board.entity.UserBoardMatches;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserBoardMatchesRepository extends JpaRepository<UserBoardMatches, Long>, UserBoardMatchesRepositoryCustom {
+public interface UserBoardMatchesRepository extends JpaRepository<UserBoardMatches, Long>,
+    UserBoardMatchesRepositoryCustom {
 
-    Optional<UserBoardMatches> findByBoardIdAndUserId(Long boardId, Long invitedUser);
+  Optional<UserBoardMatches> findByBoardIdAndUserId(Long boardId, Long invitedUser);
+
+  Optional<List<UserBoardMatches>> findByUserId(Long userId);
 
 }
