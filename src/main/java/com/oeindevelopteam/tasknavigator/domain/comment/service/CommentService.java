6de57961bc04637ae1ab3ffb3b684c8a4cf9e7f8
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-15 18:27:42 +0900

    ✨ update - #72 댓글 조회 로직 구현

--- src/main/java/com/oeindevelopteam/tasknavigator/domain/comment/service/CommentService.java
+++ src/main/java/com/oeindevelopteam/tasknavigator/domain/comment/service/CommentService.java
@@ -5,7 +5,12 @@ import com.oeindevelopteam.tasknavigator.domain.card.service.CardService;
 import com.oeindevelopteam.tasknavigator.domain.comment.dto.CommentRequestDto;
 import com.oeindevelopteam.tasknavigator.domain.comment.dto.CommentResponseDto;
 import com.oeindevelopteam.tasknavigator.domain.comment.entity.Comment;
+import com.oeindevelopteam.tasknavigator.domain.comment.repository.CommentRepository;
 import com.oeindevelopteam.tasknavigator.domain.user.entity.User;
+import com.oeindevelopteam.tasknavigator.global.exception.CustomException;
+import com.oeindevelopteam.tasknavigator.global.exception.ErrorCode;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
   private final CardService cardService;
+  private final CommentRepository commentRepository;
 
   @Transactional
   public CommentResponseDto createComment(Long cardId, User user,
@@ -31,5 +37,20 @@ public class CommentService {
     card.addComment(comment);
 
     return new CommentResponseDto(comment);
+  }
+
+  public List<CommentResponseDto> getComments(Long cardId, User user) {
+    Card card = cardService.getCardById(cardId);
+
+    if (!cardService.findUserisAdmin(user)) {
+      Long boardId = card.getBoardId();
+      cardService.checkUserPermission(user, boardId);
+    }
+    List<Comment> comments = commentRepository.findAllByCard(card)
+        .orElseThrow(() -> new CustomException(
+            ErrorCode.COMMENT_NOT_FOUND));
+
+    return comments.stream().map(comment -> new CommentResponseDto(comment))
+        .collect(Collectors.toList());
   }
 }
